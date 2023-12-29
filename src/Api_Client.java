315b1a8237dd4f9d5bae0d069401551ed6622a04
ch.lee <ch.lee@coinone.com>
2023-12-29 10:25:52 +0900

    init commit

---
+++ src/Api_Client.java
@@ -0,0 +1,205 @@
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+
+
+@SuppressWarnings("unused")
+public class Api_Client {
+
+    protected String api_url = "https://api.bithumb.com";
+    protected String api_key;
+    protected String api_secret;
+
+    public Api_Client(String api_key, String api_secret) {
+        this.api_key = api_key;
+        this.api_secret = api_secret;
+    }
+
+    private String usecTime() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    private String request(String strHost, String strMemod, HashMap<String, String> rgParams,
+        HashMap<String, String> httpHeaders) {
+        String response = "";
+
+        if (strHost.startsWith("https://")) {
+            HttpRequest request = HttpRequest.get(strHost);
+            request.trustAllCerts();
+            request.trustAllHosts();
+        }
+
+        if (strMemod.equalsIgnoreCase("HEAD")) {
+        } else {
+            HttpRequest request;
+
+            if (strMemod.equalsIgnoreCase("POST")) {
+
+                request = new HttpRequest(strHost, "POST");
+                request.readTimeout(10000);
+
+                if (httpHeaders != null && !httpHeaders.isEmpty()) {
+                    httpHeaders.put("api-client-type", "2");
+                    request.headers(httpHeaders);
+                }
+                if (rgParams != null && !rgParams.isEmpty()) {
+                    request.form(rgParams);
+                }
+            } else {
+                request = HttpRequest.get(strHost
+                    + Util.mapToQueryString(rgParams));
+                request.readTimeout(10000);
+
+            }
+
+            if (request.ok()) {
+                response = request.body();
+            } else {
+                response = "error : " + request.code() + ", message : "
+                    + request.body();
+            }
+            request.disconnect();
+        }
+
+        return response;
+    }
+
+    public static String encodeURIComponent(String s) {
+        String result;
+
+        try {
+            result = URLEncoder.encode(s, "UTF-8")
+                .replaceAll("\\+", "%20")
+                .replaceAll("\\%21", "!")
+                .replaceAll("\\%27", "'")
+                .replaceAll("\\%28", "(")
+                .replaceAll("\\%29", ")")
+                .replaceAll("\\%26", "&")
+                .replaceAll("\\%3D", "=")
+                .replaceAll("\\%7E", "~");
+        }
+
+        catch (UnsupportedEncodingException e) {
+            result = s;
+        }
+
+        return result;
+    }
+
+    private HashMap<String, String> getHttpHeaders(String endpoint, HashMap<String, String> rgData, String apiKey,
+        String apiSecret) {
+
+        String strData = Util.mapToQueryString(rgData).replace("?", "");
+        String nNonce = usecTime();
+
+        strData = strData.substring(0, strData.length() - 1);
+
+        strData = encodeURIComponent(strData);
+
+        HashMap<String, String> array = new HashMap();
+
+        String str = endpoint + ";" + strData + ";" + nNonce;
+
+        String encoded = asHex(hmacSha512(str, apiSecret));
+        array.put("Api-Key", apiKey);
+        array.put("Api-Sign", encoded);
+        array.put("Api-Nonce", nNonce);
+
+        return array;
+
+    }
+
+    private static final String DEFAULT_ENCODING = "UTF-8";
+    private static final String HMAC_SHA512 = "HmacSHA512";
+
+    public static byte[] hmacSha512(String value, String key) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(
+                key.getBytes(DEFAULT_ENCODING),
+                HMAC_SHA512);
+
+            Mac mac = Mac.getInstance(HMAC_SHA512);
+            mac.init(keySpec);
+
+            final byte[] macData = mac.doFinal(value.getBytes());
+            byte[] hex = new Hex().encode(macData);
+
+            return hex;
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String asHex(byte[] bytes) {
+        return new String(Base64.encodeBase64(bytes));
+    }
+
+    @SuppressWarnings("unchecked")
+    public String callApiPost(String endpoint, HashMap<String, String> params) {
+        String rgResultDecode;
+        HashMap<String, String> rgParams = new HashMap();
+        rgParams.put("endpoint", endpoint);
+
+        if (params != null) {
+            rgParams.putAll(params);
+        }
+
+        String api_host = api_url + endpoint;
+        HashMap<String, String> httpHeaders = getHttpHeaders(endpoint, rgParams, api_key, api_secret);
+
+        rgResultDecode = request(api_host, "POST", rgParams, httpHeaders);
+
+//        if (!rgResultDecode.startsWith("error")) {
+//            HashMap<String, String> result;
+//            try {
+//                result = new ObjectMapper().readValue(rgResultDecode,
+//                    HashMap.class);
+//
+//			System.out.println(result.get("status"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return rgResultDecode;
+    }
+
+    @SuppressWarnings("unchecked")
+    public String callApiGet(String endpoint, HashMap<String, String> params) {
+        String rgResultDecode;
+        HashMap<String, String> rgParams = new HashMap();
+        rgParams.put("endpoint", endpoint);
+
+        if (params != null) {
+            rgParams.putAll(params);
+        }
+
+        String api_host = api_url + endpoint;
+        HashMap<String, String> httpHeaders = getHttpHeaders(endpoint, rgParams, api_key, api_secret);
+
+        rgResultDecode = request(api_host, "GET", rgParams, httpHeaders);
+
+//        if (!rgResultDecode.startsWith("error")) {
+//            HashMap<String, String> result;
+//            try {
+//                result = new ObjectMapper().readValue(rgResultDecode,
+//                    HashMap.class);
+//
+//			System.out.println(result.get("status"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return rgResultDecode;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,187 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class Main {
+
+    static final int MINIMUM_TICK = 1000;
+    static final String BID_AMOUNT = "0.0005";
+    static final String ASK_AMOUNT = "0.0005";
+	static final int sleep = 100;
+	static String connectKey;
+	static String secretKey;
+    static ObjectMapper om = new ObjectMapper();
+
+    public static void main(String args[]) {
+
+		System.out.println("[connectKey]를 입력하세요(엔터)");
+		connectKey = new Scanner(System.in).nextLine();
+		System.out.println("[secretKey]를 입력하세요(엔터)");
+		secretKey = new Scanner(System.in).nextLine();
+
+		if (wasExpired()) {
+			System.out.println("사용기한이 만료되어 종료됩니다.");
+		}
+
+//		clearOrder();
+        order();
+
+    }
+
+	private static boolean wasExpired() {
+		LocalDateTime endDate = LocalDateTime.parse("2024-01-31 23:59:59", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		return LocalDateTime.now().isAfter(endDate);
+	}
+
+    private static void order() {
+        Api_Client api = new Api_Client(connectKey,
+            secretKey);
+
+        HashMap<String, String> rgParamsOrderbook = new HashMap();
+        rgParamsOrderbook.put("count", "5");
+
+		int count = 0;
+		while (true) {
+            try {
+                System.out.println("count: " + count);
+                String result = api.callApiGet("/public/orderbook/BTC_KRW", rgParamsOrderbook);
+                Map<String, Object> map = om.readValue(result, Map.class);
+                Map<String, Object> data = (Map) map.get("data");
+                List<Map<String, String>> bids = (List) data.get("bids");
+                List<Map<String, String>> asks = (List) data.get("asks");
+                Integer bidPrice = Integer.parseInt(bids.get(0).get("price")) + MINIMUM_TICK;
+                Integer askPrice = Integer.parseInt(asks.get(0).get("price")) - MINIMUM_TICK;
+
+				if (bidPrice == Integer.parseInt(asks.get(0).get("price"))) {
+					bidPrice = Integer.parseInt(bids.get(0).get("price"));
+				}
+				if (askPrice == Integer.parseInt(bids.get(0).get("price"))) {
+					askPrice = Integer.parseInt(asks.get(0).get("price"));
+				}
+
+                HashMap<String, String> rgParams = new HashMap();
+                rgParams.put("order_currency", "BTC");
+                rgParams.put("payment_currency", "KRW");
+                rgParams.put("units", BID_AMOUNT);
+                rgParams.put("type", "bid");
+                rgParams.put("price", String.format("%d", bidPrice));
+				Map<String, Object> bidResult = om.readValue(api.callApiPost("/trade/place", rgParams), Map.class);
+				if ("5600".equals(bidResult.get("status"))) {
+					cancelBid();
+				}
+
+                rgParams.put("units", ASK_AMOUNT);
+                rgParams.put("type", "ask");
+                rgParams.put("price", String.format("%d", askPrice));
+                Map<String, Object> askResult = om.readValue(api.callApiPost("/trade/place", rgParams), Map.class);
+				if ("5600".equals(askResult.get("status"))) {
+					cancelAsk();
+				}
+                Thread.sleep(sleep);
+            } catch (Exception e) {
+				cancelOrder();
+//                e.printStackTrace();
+            } finally {
+				count++;
+			}
+        }
+    }
+
+    private static void cancelBid() {
+        Api_Client api = new Api_Client(connectKey,
+            secretKey);
+
+        HashMap<String, String> rgParams = new HashMap();
+        rgParams.put("order_currency", "BTC");
+        rgParams.put("payment_currency", "KRW");
+
+        try {
+            String result = api.callApiPost("/info/orders", rgParams);
+            Map<String, Object> map = om.readValue(result, Map.class);
+            List<Map<String, String>> data = (List) map.get("data");
+            for (Map<String, String> ele : data) {
+                String orderId = ele.get("order_id");
+                String type = ele.get("type");
+				if (type.equals("ask")) {
+					continue;
+				}
+                rgParams.put("order_id", orderId);
+                rgParams.put("type", type);
+                api.callApiPost("/trade/cancel", rgParams);
+				System.out.println("cancel_bid: " + orderId);
+            }
+        } catch (Exception e) {
+            // ignore
+//            e.printStackTrace();
+        }
+    }
+
+	private static void cancelAsk() {
+
+		Api_Client api = new Api_Client(connectKey,
+			secretKey);
+
+		HashMap<String, String> rgParams = new HashMap();
+		rgParams.put("order_currency", "BTC");
+		rgParams.put("payment_currency", "KRW");
+
+		try {
+			String result = api.callApiPost("/info/orders", rgParams);
+			Map<String, Object> map = om.readValue(result, Map.class);
+			List<Map<String, String>> data = (List) map.get("data");
+			for (Map<String, String> ele : data) {
+				String orderId = ele.get("order_id");
+				String type = ele.get("type");
+				if (type.equals("bid")) {
+					continue;
+				}
+				rgParams.put("order_id", orderId);
+				rgParams.put("type", type);
+				api.callApiPost("/trade/cancel", rgParams);
+				System.out.println("cancel_ask: " + orderId);
+			}
+		} catch (Exception e) {
+			// ignore
+//            e.printStackTrace();
+		}
+	}
+
+	private static void cancelOrder() {
+
+		Api_Client api = new Api_Client(connectKey,
+			secretKey);
+
+		HashMap<String, String> rgParams = new HashMap();
+		rgParams.put("order_currency", "BTC");
+		rgParams.put("payment_currency", "KRW");
+
+		try {
+			String result = api.callApiPost("/info/orders", rgParams);
+			Map<String, Object> map = om.readValue(result, Map.class);
+			List<Map<String, String>> data = (List) map.get("data");
+			for (Map<String, String> ele : data) {
+				String orderId = ele.get("order_id");
+				String type = ele.get("type");
+				rgParams.put("order_id", orderId);
+				rgParams.put("type", type);
+				api.callApiPost("/trade/cancel", rgParams);
+				System.out.println("cancel_order: " + orderId);
+			}
+		} catch (Exception e) {
+			// ignore
+//            e.printStackTrace();
+		}
+	}
+}
+
